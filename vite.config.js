8681378f4d9833d5cb01a6bcb95df7eec9b7dfa4
YZF_0926 <yang_ze_fan_123@qq.com>
2025-09-10 18:05:12 +0800

    element主题色的定制

--- vite.config.js
+++ vite.config.js
@@ -18,14 +18,26 @@ export default defineConfig({
     AutoImport({
       resolvers: [ElementPlusResolver()],
     }),
+    
     Components({
-      resolvers: [ElementPlusResolver()],
-    })
+      resolvers: [
+        //1. 增加{  importStyle: "sass" }，配置elementPlus采用sass样式配色系统
+        ElementPlusResolver({  importStyle: "sass" }),
+      ],
+    }),
   ],
   resolve: {
     //实际的路径转换，将@转src
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
+    },
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        // 2. 自动导入定制化样式文件进行样式覆盖
+        additionalData: `@use "@/styles/element/index.scss" as *;`,
+      },
     },
   },
 })
