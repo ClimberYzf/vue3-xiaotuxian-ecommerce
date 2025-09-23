dc013ca43aaf13816d58491d8536efbb2927bab6
YZF_0926 <yang_ze_fan_123@qq.com>
2025-09-23 15:40:28 +0800

    layout-渲染一级导航列表

--- src/main.js
+++ src/main.js
@@ -7,14 +7,14 @@ import router from './router'
 //引入初始化样式文件
 import '@/styles/common.scss'
 
-//测试接口函数
-import { getCategory } from '@/apis/testAPI'
-getCategory().then(
-    //打印promise对象
-    res => {
-        console.log(res)
-    }
-)
+//测试接口函数-接口暂时无法访问，运行时控制台会报错，先注释掉
+// import { getCategory } from '@/apis/testAPI'
+// getCategory().then(
+//     //打印promise对象
+//     res => {
+//         console.log(res)
+//     }
+// )
 
 const app = createApp(App)
 
