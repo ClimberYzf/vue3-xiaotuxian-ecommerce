3c55ce6dce8e48e810ad27224af92dfd427a31dd
YZF_0926 <yang_ze_fan_123@qq.com>
2025-09-24 17:19:11 +0800

    home-面板组件封装

---
+++ src/views/Home/components/HomePanel.vue
@@ -0,0 +1,53 @@
+<script setup>
+defineProps({
+    //主标题
+    title:{
+        type:String
+    },
+    //副标题
+    subTitle:{
+        type:String
+        }
+})
+</script>
+<template>
+  <div class="home-panel">
+    <div class="container">
+      <div class="head">
+        <!-- 主标题和副标题 -->
+        <h3>
+        {{ title }}<small>{{subTitle}}</small>
+        </h3>
+      </div>
+      <!-- 主体内容区域,用插槽 -->
+      <slot />
+    </div>
+  </div>
+</template>
+
+<style scoped lang='scss'>
+.home-panel {
+  background-color: #fff;
+
+  .head {
+    padding: 40px 0;
+    display: flex;
+    align-items: flex-end;//子元素在交叉轴(垂直方向)的末端对齐（底部对齐）
+
+    h3 {
+      flex: 1;//让弹性子元素按比例占据容器的剩余空间.这里应该是自动占剩余的空间.
+      font-size: 32px;
+      font-weight: normal;
+      margin-left: 6px;
+      height: 35px;
+      line-height: 35px;
+
+      small {
+        font-size: 16px;
+        color: #999;
+        margin-left: 20px;
+      }
+    }
+  }
+}
+</style>
--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -4,6 +4,7 @@
     import HomeNew from './components/HomeNew.vue';
     import HomeProduct from './components/HomeProduct.vue';
     import HomeBanner  from './components/HomeBanner.vue';
+    import HomePanel from './components/HomePanel.vue'; 
 </script>
 <template>
     <div class="container">
@@ -13,4 +14,15 @@
     <HomeNew />
     <HomeHot />
     <HomeProduct />
+    <!-- 测试面板组件 -->
+    <HomePanel title="新鲜好物" sub-title="新鲜好物 每日必推">
+        <div>
+            我是新鲜好物的插槽
+        </div>
+    </HomePanel>
+    <HomePanel title="人气推荐" sub-title="人气推荐 精选好货">
+        <div>
+            我是人气推荐的插槽
+        </div>
+    </HomePanel>
 </template>
