286a991b1d141736745392c9818d0ed9287af307
YZF_0926 <yang_ze_fan_123@qq.com>
2025-10-14 00:02:36 +0800

    详情热榜-基础组件封装与基础组件获取渲染

--- src/apis/detail.js
+++ src/apis/detail.js
@@ -8,3 +8,14 @@ export const getDetail = (id) => {
         } 
     }) 
 }
+
+export const getHotGoodsAPI = ({id, type, limit=3}) => {
+    return httpInstance({
+        url: "/goods/hot",
+        params:{
+            id,
+            type, //1表示24小时热榜，2表示周热榜
+            limit
+        }
+    })
+}
--- src/views/Detail/index.vue
+++ src/views/Detail/index.vue
@@ -2,6 +2,7 @@
 import {getDetail} from '@/apis/detail'
 import {ref, onMounted } from 'vue'
 import {useRoute} from 'vue-router'
+import DetailHot from './components/DetailHot.vue'
 
 const detailData = ref({})
 const route = useRoute()
@@ -128,7 +129,10 @@ onMounted(() => getDetailData())
             </div>
             <!-- 24热榜+专题推荐 -->
             <div class="goods-aside">
-             
+               <!-- 24小时 -->
+              <DetailHot />
+              <!-- 周 -->
+               <DetailHot />
             </div>
           </div>
         </div>
