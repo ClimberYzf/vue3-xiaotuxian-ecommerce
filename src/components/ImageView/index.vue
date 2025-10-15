b8086e88c9fd30e4dd7f9a50934499a40c23e1e4
YZF_0926 <yang_ze_fan_123@qq.com>
2025-10-15 10:53:45 +0800

    放大镜效果-滑块跟随鼠标移动

--- src/components/ImageView/index.vue
+++ src/components/ImageView/index.vue
@@ -1,7 +1,9 @@
 <!-- components文件夹下放和业务不强相关的通用型组件 -->
 
 <script setup>
-import { ref } from 'vue'
+import { ref , watch} from 'vue'
+import { useMouseInElement } from '@vueuse/core'
+
 // 图片列表
 const imageList = [
   "https://yanxuan-item.nosdn.127.net/d917c92e663c5ed0bb577c7ded73e4ec.png",
@@ -11,22 +13,52 @@ const imageList = [
   "https://yanxuan-item.nosdn.127.net/f881cfe7de9a576aaeea6ee0d1d24823.jpg"
 ]
 
-//小图切换大图显示
+//1.小图切换大图显示
 const activeIndex = ref(0)
 const enterhandler = (i) => {
     activeIndex.value = i
 }
+
+//2.获取鼠标相对位置
+
+const target = ref(null)
+const {elementX, elementY, isOutside} = useMouseInElement(target)
+
+//3.控制滑块跟随鼠标移动（监听elementX/Y变化，一旦变化重新设置left/top）
+const left = ref(0)
+const top = ref(0)
+watch([elementX, elementY], () => {
+    //有效范围内控制滑块距离
+    //横向
+    if (elementX.value > 100 && elementX.value < 300) {
+      left.value = elementX.value - 100
+    }
+    //纵向
+    if (elementY.value > 100 && elementY.value < 300) {
+      top.value = elementY.value - 100
+    }
+    //处理边界
+    if (elementX.value > 300) {left.value = 200}
+    if (elementX.value < 100) {left.value = 0}
+    if (elementY.value > 300) {top.value = 200}
+    if (elementY.value < 100) {top.value = 0}
+
+})
+
 </script>
 
 
 <template>
+  <!-- {{  elementX }}, {{elementY}}, {{isOutside}}  -->
   <div class="goods-image">
-
     <!-- 左侧大图-->
+     <!-- 
+    ref="target" 是用于获取 DOM 元素或组件实例的引用方式，
+    相当于给这个 <div> 元素起了一个 “标识名”，方便在脚本中直接访问它。-->
     <div class="middle" ref="target">
       <img :src="imageList[activeIndex]" alt="" />
       <!-- 蒙层小滑块 -->
-      <div class="layer"  :style="{ left: `0px`, top: `0px` }"></div>
+      <div class="layer"  :style="{ left: `${left}px`, top: `${top}px` }"></div>
     </div>
     <!-- 小图列表 -->
     <ul class="small">
