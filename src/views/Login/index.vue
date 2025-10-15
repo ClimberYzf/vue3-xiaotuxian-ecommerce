46e3271fd820259bf3783b168d4ac6d9e557a690
YZF_0926 <yang_ze_fan_123@qq.com>
2025-10-15 17:45:44 +0800

    登录-表单校验

--- src/views/Login/index.vue
+++ src/views/Login/index.vue
@@ -1,5 +1,22 @@
 <script setup>
-
+//表单校验（账号名+密码）
+import { ref } from 'vue'   
+//1.准备表单对象
+const form = ref({
+    account: '',
+    password: ''
+})
+//2.准备规则对象
+const rules = {
+    account: [
+        // trigger:'blur'——指定验证触发时机的配置项，意思是 “当表单元素失去焦点时，才执行验证逻辑”。
+        {required:true,message:'用户名不能为空',trigger:'blur'}
+    ],
+    password: [
+        {required:true,message:'密码不能为空',trigger:'blur'},
+        {min:6,max:14,message:'密码长度为6-14个字符',trigger:'blur'}
+    ]
+}
 </script>
 
 <template>
@@ -23,16 +40,18 @@
         </nav>
         <div class="account-box">
           <div class="form">
-            <el-form               
+            <el-form   
+            :model="form" 
+            :rules="rules"           
               label-position="right"
               label-width="60px"
               status-icon
             >
-              <el-form-item  label="账户">
-                <el-input  />
+              <el-form-item prop="account"  label="账户">
+                <el-input  v-model="form.account"/>
               </el-form-item>
-              <el-form-item  label="密码">
-                <el-input   />
+              <el-form-item prop="password"  label="密码">
+                <el-input  v-model="form.password" />
               </el-form-item>
               <el-form-item label-width="22px">
                 <el-checkbox size="large"  >
