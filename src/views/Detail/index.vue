c5141bb130b02d0c3cd1f49696b444d80e4363cd
YZF_0926 <yang_ze_fan_123@qq.com>
2025-10-19 23:34:03 +0800

    本地购物车-添加购物车实现

--- src/views/Detail/index.vue
+++ src/views/Detail/index.vue
@@ -5,6 +5,7 @@ import {useRoute} from 'vue-router'
 import DetailHot from './components/DetailHot.vue'
 // import ImageView from '@/components/ImageView/index.vue'
 // import XtxSku from '@/components/XtxSku/index.vue'
+import { useCartStore } from '@/stores/cartStore' 
 const detailData = ref({})
 const route = useRoute()
 // console.log("详情页")
@@ -19,11 +20,43 @@ const getDetailData = async () => {
 onMounted(() => getDetailData())
 
 //sku规格被操作时
+let skuObj = {}
 const skuChange = (sku) => {
   console.log("skuChange：",sku)
+  skuObj = sku
 }
 
+//count
+const count = ref(1)
+const countChange = (count) => {
+  console.log(count)
+}
 
+const cartStore = useCartStore()
+//添加购物车
+const addCart = () => {
+  if (skuObj.skuId) {
+    //规则已经选择 触发action
+    cartStore.addCart({
+       id:  detailData.value.id,
+       name: detailData.value.name,
+       picture: detailData.value.mainPictures[0],
+       price: detailData.value.price,
+       count: count.value,
+       skuId: skuObj.skuId,
+       attrsText: skuObj.specsText,
+       selected: true
+    }
+      
+    )
+
+
+  } else {
+    //规格没有选择 提示用户
+    ElMessage.warning('请选择规格')
+  }
+
+}
 </script>
 
 <template>
@@ -104,10 +137,11 @@ const skuChange = (sku) => {
               <!-- sku组件 -->
                <XtxSku :goods="detailData" @change="skuChange"/>
               <!-- 数据组件 -->
-               
+               <!-- element plus的input-number-数字输入框 -->
+               <el-input-number v-model="count" @change="countChange" />
               <!-- 按钮组件 -->
-              <div>
-                <el-button size="large" class="btn"  >
+              <div>                
+                <el-button size="large" class="btn" @click="addCart" >
                   加入购物车
                 </el-button>
               </div>
